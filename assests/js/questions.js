8d89e1494c0b7fa685c00d0494ab57078b2af9fc
Grace <cgracesharpe@gmail.com>
2022-04-19 22:29:15 -0400

    GD: quiz logic added, questions pulled from array, right and wrong answers validated

---
+++ assests/js/questions.js
@@ -0,0 +1,54 @@
+// array of questions
+var quizQuestions = [
+    {
+        question: "commonly used data types do NOT include",
+        choices: ['strings', 'booleons', 'alerts', 'numbers'],
+        answer:  'alerts'
+    },
+    {
+        question: "what is the default behavior called that is used to move declarations to the top of the current scope?",
+        choices: ['hoisting', 'arranging', 'sorting', 'jumping'],
+        answer: 'hoisting'
+    },
+    {
+        question: "in javascript, what is used in conjunction with HTML to 'react' to certain elements",
+        choices: ['events', 'regex', 'condition', 'booleon'],
+        answer: 'events'
+    },
+    {
+        question: "in javascript, what element is used to store and manipulate text, usually in multiples?",
+        choices: ['recorders', 'arrays', 'variables', 'strings'],
+        answer: 'strings'
+    },
+    {
+        question: "what is considered the most popular programming language in the world?",
+        choices: ['swift', 'HTML', 'javascript', 'ruby'],
+        answer: 'javascript'
+    },
+    {
+        question: "what is the name of the statement that is used to exit or end a loop?",
+        choices: ['falter statement','break statement','conditional statement','close statement',],
+        answer: 'break statement'
+    },
+    {
+        question: "commonly used data types do NOT include",
+        choices: ['strings','booleons','alerts','numbers'],
+        answer: 'alerts'
+    },
+    {
+        question: "any field declared with the keyword _____ is constant",
+        choices: ['constant', 'static', 'final', 'const'],
+        answer: 'const'
+    },
+    {
+        question: "the condition of an IF/ELSE statement is enclosed in",
+        choices: ['curly braces', 'sing quotes', 'parenthesis', 'double quotes'],
+        answer: 'parenthesis'
+    },
+
+    {
+        question: "arrays in javascript can be used to store ",
+        choices: ['booleons', 'numbers and strings', 'other arrays', 'all of the above'],
+        answer: 'all of the above'
+    }
+]
--- assests/js/script.js
+++ assests/js/script.js
@@ -1,141 +1,114 @@
 // DOM element variables
-var buttonEl = document.querySelector("#start-btn");
-var quizChoiceEl = document.querySelector("#quiz-qst");
-var quizItemEl = document.createElement("li");
+var buttonEl = document.getElementById("start-btn");
+var gameIntro = document.getElementById("game-intro");
+var gameBody = document.getElementById("game-body");
+
+// quiz variables
+var questionEl = document.getElementById("question");
+var choicesEl = document.getElementById("quiz-choices");
+var feedbackEl = document.getElementById("feedback");
+let currentQuestionIndex = 0;
+
+//timer variables
 var timerEl = document.getElementById("timer");
-var time= 60;
-
-// array of questions
-var quizQuestions = [
-    {
-        question: "commonly used data types do NOT include",
-        answers: {
-            1: 'strings',
-            2: 'booleons',
-            3: 'alerts',
-            4: 'numbers',
-        },
-        correct: '3'
-    },
-    {
-        question: "what is the default behavior called that is used to move declarations to the top of the current scope?",
-        answers: {
-            1: 'hoisting',
-            2: 'arranging',
-            3: 'sorting',
-            4: 'jumping',
-        },
-        correct: '1'
-    },
-    {
-        question: "in javascript, what is used in conjunction with HTML to 'react' to certain elements",
-        answers: {
-            1: 'events',
-            2: 'regex',
-            3: 'condition',
-            4: 'boolean',
-        },
-        correct: '1'
-    },
-    {
-        question: "in javascript, what element is used to store and manipulate text, usually in multiples?",
-        answers: {
-            1: 'recorders',
-            2: 'arrays',
-            3: 'variables',
-            4: 'strings',
-        },
-        correct: '4'
-    },
-    {
-        question: "what is considered the most popular programming language in the world?",
-        answers: {
-            1: 'swift',
-            2: 'HTML',
-            3: 'javascript',
-            4: 'ruby',
-        },
-        correct: '3'
-    },
-    {
-        question: "what is the name of the statement that is used to exit or end a loop?",
-        answers: {
-            1: 'falter statement',
-            2: 'break statement',
-            3: 'conditional statement',
-            4: 'close statement',
-        },
-        correct: '3'
-    },
-    {
-        question: "commonly used data types do NOT include",
-        answers: {
-            1: 'strings',
-            2: 'booleons',
-            3: 'alerts',
-            4: 'numbers',
-        },
-        correct: '2'
-    },
-    {
-        question: "any field declared with the keyword _____ is constant",
-        answers: {
-            1: 'constant',
-            2: 'static',
-            3: 'final',
-            4: 'const',
-        },
-        correct: '4'
-    },
-    {
-        question: "the condition of an IF/ELSE statement is enclosed in",
-        answers: {
-            1: 'curly braces',
-            2: 'single quotes',
-            3: 'parenthesis',
-            4: 'double quotes',
-        },
-        correct: '3'
-    },
-    
-    {
-        question: "arrays in javascript can be used to store ",
-        answers: {
-            1: 'booleons',
-            2: 'numbers and strings',
-            3: 'other arrays',
-            4: 'all of the above',
-        },
-        correct: '4'
-    },
-]
+var time = 60;
+var timerId;
 
 
+function quizStart() {
+    //clear starter elements
+    gameIntro.setAttribute("class", "hide");
+    // un-hide quiz elements
+    gameBody.setAttribute("class", "show");
+    // start and show time to count down by one second from 60
+    timerId = setInterval(countDownTimer, 1000);
+    // get questions from getQuestions function
+    getQuestions()
+}
+function getQuestions() {
+    // get question from array
+    var currentQuestion = quizQuestions[currentQuestionIndex];
 
-function quizStart () { 
-    // TODO: find a way to implement questions and answers and timer start
-    function getQuestions(){}
-    function questionChoice(){
-     // TODO: generate right and wrong answers, validate correct
-    }
+    // display question
+    var questTitleEl = document.getElementById("quiz-qst");
+    questTitleEl.textContent = currentQuestion.question;
+
+    // clear previous question choices
+    choicesEl.innerHTML = "";
+
+    // loop over choices
+    currentQuestion.choices.forEach((element, index) =>{
+        // create new list element for each newly generated choices
+        var choiceNode = document.createElement("li");
+        choiceNode.setAttribute("id", "choices");
+        // choiceNode.setAttribute("value", choice);
+
+        choiceNode.textContent = index + 1 + ". " + element;
+
+        // event listner for answer choice
+        choiceNode.addEventListener("click", function () {
+            questionClick(quizQuestions, element)
+        });
+
+        // display on the page and append to unordered list element
+        choicesEl.appendChild(choiceNode);
+    });
 }
 
-function quizStop(){
+// click on question answer either generate new question or end quiz if final question, and deduct time for answering wrong
+function questionClick(quizQuestions, answer) {
+    // check if user guessed wrong
+    if ( answer !== quizQuestions[currentQuestionIndex].answer) {
+        // penalize 10 seconds from time/score
+        time -= 10;
+        // if, when time is penalized, and will take time to bellow zero, make time equal zero
+        if (time < 0) {
+            time = 0;
+        }
+        // display new time on page
+        timerEl.textContent = "time:" + time;
+        // feedback response should chosen question be wrong
+        feedbackEl.textContent = "Gonna be a no from me, dawg";
+    } else {
+        // else answer is correct
+        feedbackEl.textContent = "YES! You're so smart!";
+    }
+
+    // display feedback for one second
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function () {
+        feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+
+    // move to next question
+    currentQuestionIndex++;
+
+    // check if we've run out of questions
+    if (currentQuestionIndex === quizQuestions.length) {
+        quizEnd();
+    } else {
+        getQuestions();
+    }
+}
+function countDownTimer() {
+    time--;
+    timerEl.textContent = "time: " + time;
+
+    if (time <= 0) {
+
+        quizEnd();
+        
+    }
+};
+
+function quizEnd() {
+    clearInterval(timerId);
     // TODO: what happens when quiz ends?
     // localstorage option, initials and score. score = time remaining on timer if any
     // 
 }
 
-function quizTimer () {
-    timerEl.textContent = "Time:" + time;
-
-    // TODO: subtract time if incorrect answer
-
-    if (time <= 0) {
-        quizStop();
-    }
-}
 
 
 buttonEl.addEventListener("click", quizStart);
-quizItemEl.addEventListener("click", questionChoice);
+// quizItemEl.addEventListener("click", questionChoice);
